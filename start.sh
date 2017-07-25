5fd38b4bc6fa2d0e80f9c018bfa1791ee98d16a9
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-07-25 17:07:48 +0100

    Reinstated default AWS region env var in Renviron

--- start.sh
+++ start.sh
@@ -17,6 +17,9 @@ fi
 chown "${USER}:${GROUP}" /home/$USER
 export HOME=/home/$USER
 
+# Pass select root env vars to user's R environment
+echo "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" >> /usr/local/lib/R/etc/Renviron
+
 # set secure cookie key
 echo -n "${SECURE_COOKIE_KEY}" > /var/lib/rstudio-server/secure-cookie-key
 chmod 600 /var/lib/rstudio-server/secure-cookie-key
